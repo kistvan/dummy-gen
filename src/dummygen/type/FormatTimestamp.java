43d3d0c26807e18a5264c12d8f9ec92da0a17581
koji yamaguchi <yamaguchi@neopa.jp>
2012-09-12 15:10:12 +0900

    固定日付にランダム幅を持たせるように

--- src/dummygen/type/FormatTimestamp.java
+++ src/dummygen/type/FormatTimestamp.java
@@ -5,11 +5,12 @@ package dummygen.type;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.Random;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatterBuilder;
 
 /**
  * yyyy/MM/dd HH:mm:ss でフォーマットされた文字列から時間に変換するTypeValue
@@ -18,6 +19,7 @@ import org.joda.time.format.DateTimeFormatterBuilder;
 public class FormatTimestamp implements TypeValue {
 	
 	private Date date;
+	private int randRangeSec = 0;
 
 	@Override
 	public Object getValue() {
@@ -30,7 +32,20 @@ public class FormatTimestamp implements TypeValue {
 	@Override
 	public void eval(String arg) {
 		try {
-			date = DateUtils.parseDate(arg, "yyyy/MM/dd HH:mm:ss");
+			if (arg.contains(",")) {
+				//ランダム
+				String[] s = StringUtils.split(arg, ',');
+				date = DateUtils.parseDate(StringUtils.trim(s[0]), "yyyy/MM/dd HH:mm:ss");
+				randRangeSec = NumberUtils.toInt(s[1]);
+				//0以外の場合はランダムに加算する
+				if (randRangeSec == 0) {
+					return ;
+				}
+				int n = new Random().nextInt(randRangeSec);
+				date = new DateTime(date).plusSeconds(n).toDate();
+			} else {
+				date = DateUtils.parseDate(arg, "yyyy/MM/dd HH:mm:ss");
+			}
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
